09fdc0af28618be5f0b3dbb93567ecd84af6287e
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-08 21:49:50 +0530

    basic operators in javascript

---
+++ js/basicoperaters.js
@@ -0,0 +1,20 @@
+let a=20;
+let b=10;
+
+//addition
+alert("Addition is "+(a+b));
+
+//subtraction
+alert("Subtraction is "+(a-b));
+
+//multiplication 
+alert("Multiplication is "+(a*b));
+
+//division
+alert("Division is "+(a/b));
+
+//modulo division
+alert("Modulo Division is"+(a%b));
+
+//expoentional
+alert("Expoentional is "+(a**b));
