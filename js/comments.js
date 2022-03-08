de92ab37f39bea2299ceb736367a5c15051020c8
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-08 20:08:57 +0530

    types of comments in javascript

---
+++ js/comments.js
@@ -0,0 +1,13 @@
+// 1) multilined comments /*.....*/
+// 2) singlelined comments //.....
+
+/* This script is to add two numbers
+    a=20 and 
+    b=30
+*/
+
+//adding two numbers
+let a=20;
+let b=30;
+console.log("The sum is "+(a+b));
+
