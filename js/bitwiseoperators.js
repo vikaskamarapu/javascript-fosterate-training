b1adb86bfa87319288c12f30b6b7a75006244a6a
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-10 19:55:00 +0530

    bitwise and logical operators

---
+++ js/bitwiseoperators.js
@@ -0,0 +1,30 @@
+// bitwise AND operator example
+
+let a = 12; 
+let  b = 25; 
+
+result = a & b; 
+console.log(result);
+
+// bitwise OR operator example
+result = a | b; 
+console.log(result);
+
+// bitwise XOR operator example
+result = a ^ b; 
+console.log(result);
+
+// bitwise NOT operator example
+result = ~b;
+console.log(result);
+
+a = 8;
+b = 1;
+
+//bitwise left shift operator example
+result = a << b;
+console.log(result);
+
+//bitwise right shift operator example
+result = a >> b;
+console.log(result); 
---
+++ js/logicaloperators.js
@@ -0,0 +1,33 @@
+let a = 5;
+
+// equal to operator
+console.log(a == 5);
+console.log(a == '5');
+
+a = 3;
+let b = 'hello';
+
+// not equal operator
+console.log(a != 2);
+console.log(b != 'Hello');
+
+a = 2;
+
+// strict equal operator
+console.log(a === 2);
+console.log(a === '2');
+
+a = true;
+b = false;
+
+// logical AND
+console.log(a && a);
+console.log(a && b);
+
+// logical OR
+console.log(a || b);
+console.log(b || b);
+
+// logical NOT
+console.log(!a);
+console.log(!b); 
