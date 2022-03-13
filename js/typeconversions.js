66984a3926fffbab92e227dbdf7bf33692a43b27
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-13 22:04:49 +0530

    type conversions

---
+++ js/typeconversions.js
@@ -0,0 +1,24 @@
+//type conversions
+
+//Strings to Numbers
+let s="3.14";
+console.log(Number(s));
+
+//Numbers to Strings
+let n=3.14
+console.log(String(n));
+
+//Dates to numbers
+let d= new Date();
+console.log(Number(d))
+
+//Dates to Strings
+console.log(String(Date()));
+
+//Booleans to Numbers
+console.log(Number(false));
+console.log(Number(true));
+
+//Booleans to Strings
+console.log(String(false));
+console.log(String(true));
