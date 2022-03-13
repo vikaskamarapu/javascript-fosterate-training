6196b834b662099a7047a55f21c8890e08ab053c
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-13 21:12:05 +0530

    if statements

---
+++ js/conditional.js
@@ -0,0 +1,32 @@
+let hour=15
+//if statement
+if (hour < 18) 
+{
+    console.log("Good day");
+}
+
+hour=19
+//if else statement
+if (hour < 18) 
+{
+    console.log("Good day")
+} 
+else 
+{
+    console.log("Good evening");
+}
+
+hour=21
+//else if statement
+if (hour < 10) 
+{
+    console.log("Good morning");
+} 
+else if (hour < 20) 
+{
+    console.log("Good day");
+} 
+else 
+{
+    console.log("Good evening");
+}
