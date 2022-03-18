f0fc43cce5fb3610b40ddc45c7d0b4784f1199bd
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-18 13:08:44 +0530

    arrow functions in javascript

---
+++ js/arrowfunctions.js
@@ -0,0 +1,11 @@
+//non parameterized arrow function
+let fun1=()=>console.log("Hello World!");
+
+//parameterized arrow function
+let fun2=(para1,para2)=>console.log(para1+para2);
+
+//calling function 1
+fun1();
+
+//calling function 2
+fun2("world war ",3);
