4296fc1cf253da999061185ad8e0e93e2f7fa76c
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-18 12:29:26 +0530

    Arrays in javascript

---
+++ js/arrays.js
@@ -0,0 +1,22 @@
+//array declaration
+let ar=new Array();
+let arr=[];
+
+//initialization
+arr=["apple","ball","cat"];
+ar[0]="A";
+ar[1]="E";
+ar[2]="I";
+ar[3]="O";
+ar[4]="U";
+
+//printing the array
+for(let i of ar)
+{
+    console.log(i)
+}
+
+for(let i of arr)
+{
+    console.log(i)
+}
