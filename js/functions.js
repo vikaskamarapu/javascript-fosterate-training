356f47f956d0b7f215efa440379ad28b30bb63f1
vikaskamarapu <vikaskamarapu2001@gmail.com>
2022-03-18 12:55:36 +0530

    functions in javascript

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+//non-parameterized functions
+function showMessage1()
+{
+    console.log("Hello World!");
+}
+
+//parameterized functions
+function showMessage2(para1,para2)
+{
+    console.log(para1);
+    console.log(para2);
+}
+
+//calling function1
+showMessage1();
+
+//calling functon2
+showMessage2("world war",3);
